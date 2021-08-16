41c840cfb2cea76a5f73bfde9c767abc725cd14a
amir <amir.sediqi69@gmail.com>
2021-08-16 14:28:42 +0200

    Initial Commit

---
+++ Shared/firebase_authApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebase_authApp.swift
+//  Shared
+//
+//  Created by Amir Sediqi on 2021-08-16.
+//
+
+import SwiftUI
+
+@main
+struct firebase_authApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
